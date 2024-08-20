195dbe6f2ce2d46ba2ecba761e4e74c07fc232de
Senith <senithdilitha@gmail.com>
2024-08-20 09:49:49 +0530

    Add Book List Service

---
+++ BookListService/BookListService.Domain/Interfaces/IBookService.cs
@@ -0,0 +1,8 @@
+﻿using BookListService.Domain.Entities;
+
+namespace BookListService.Domain.Interfaces;
+
+public interface IBookService
+{
+    Task<IEnumerable<Book>> GetBooksAsync(string genre, string author, int page, int pageSize);
+}
