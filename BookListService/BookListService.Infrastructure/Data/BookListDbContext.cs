52540d0e063100f1d835be05d12d7d8184bbb17d
Senith <senithdilitha@gmail.com>
2024-08-21 09:21:35 +0530

    Add Changes for Entity Framework implementation

---
+++ BookListService/BookListService.Infrastructure/Data/BookListDbContext.cs
@@ -0,0 +1,39 @@
+﻿using BookListService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListService.Infrastructure.Data;
+
+public class BookListDbContext : DbContext
+{
+    public BookListDbContext(DbContextOptions<BookListDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = 1,
+                Title = "The Pragmatic Programmer",
+                Author = "Andrew Hunt, David Thomas",
+                Genre = "Programming",
+                Price = 39.99M,
+                PublishedDate = new DateTime(1999, 10, 20)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Clean Code",
+                Author = "Robert C. Martin",
+                Genre = "Programming",
+                Price = 49.99M,
+                PublishedDate = new DateTime(2008, 8, 1)
+            }
+        );
+    }
+}
---
+++ BookListService/BookListService.Infrastructure/Data/BookSqlServerRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+using BookListService.Domain.Entities;
+using BookListService.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListService.Infrastructure.Data
+{
+    public class BookSqlServerRepository : IBookRepository
+    {
+        private readonly BookListDbContext _context;
+
+        public BookSqlServerRepository(BookListDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync(Expression<Func<Book, bool>> filter, int page = 1, int pageSize = 10)
+        {
+            return await _context.Books
+                .Where(filter)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+    }
+}
