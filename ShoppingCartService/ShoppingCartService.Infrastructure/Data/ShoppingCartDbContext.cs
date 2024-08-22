a36ff9eee804836b6d76b9c570928eb6b7be66b6
Senith <senithdilitha@gmail.com>
2024-08-22 19:59:43 +0530

    Update Shopping Cart Service

---
+++ ShoppingCartService/ShoppingCartService.Infrastructure/Data/ShoppingCartDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCartService.Domain.Entities;
+
+namespace ShoppingCartService.Infrastructure.Data;
+
+public class ShoppingCartDbContext : DbContext
+{
+    public ShoppingCartDbContext(DbContextOptions<ShoppingCartDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<CartItem> CartItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CartItem>().ToTable("CartItems");
+    }
+}
